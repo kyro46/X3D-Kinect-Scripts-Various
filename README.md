28bd70d2bd843a7f221e8c7cae0c0b23ba6f1bc1
Christoph Jobst <cjobst@wifa.uni-leipzig.de>
2013-03-05 03:33:26 +0100

    [feature] 2D Cursor and TourchSensor + Cave, [feature] XHTML-Model

--- README.md
+++ README.md
@@ -14,20 +14,29 @@ Nodeskeleton_+_Cursor.x3d
 Nodeskeleton.js
 * Renders a Skeleton as used in the Kinect SDK
 * Used in
-** Nodeskeleton_+_Cursor.x3d
+ ** Nodeskeleton_+_Cursor.x3d
 
 RightHand3DCursor.js
 * Uses JointPositions and delivers a single SFVec3f for translationspurposes
 * Used in
-** Nodeskeleton_+_Cursor.x3d
-** 2DCursor.x3d with slight adjusments
+ ** Nodeskeleton_+_Cursor.x3d
+ ** 2DCursor.x3d with slight adjusments
+ ** 2DCursorInteractive.x3d
 
 2DCursor.x3d
 * Creates a 2D-Cursor controlled via kinect to interact with the sensors in the scene (based on ir_userbody.wrl from IR-Tutorials)
 
-TouchDebugOutput
+TouchDebugOutput.js
 * Prints the state of a sensor, non generic
 * Used in
-** 2DCursor.x3d
+ ** 2DCursor.x3d
+ ** 2DCursorInteractive.x3d
+
+2DCursorInteractive.x3d
+* Previous shown 2D-Cursor plus interactive adding and deleting rotating Objects (see dynamic_box_04.x3d in IR-Tutorials)
+
+Cave-head-flystick.x3d
+* Wraps the scene and configures the VR-Cluster (©Richard Müller, IWI Leipzig)
+
 
 Next: Interaction with animated context menu
---
+++ src/Kinect SDK/2D Cursor Interaction/RightHand3DCursor.js
@@ -0,0 +1,38 @@
+			
+			var joints = new Array();
+
+			function initialize() 
+			{
+				//print("RightHand3DCursor START");
+				//cursor.render = false;
+				
+				 for(var i=0; i<24; ++i)
+                    {
+                        var transform = Browser.currentScene.createNode("Transform");
+                        var text = Browser.currentScene.createNode("Text");
+						text.string = new MFString("RightHand3DCursor");
+                        transform.children[0] = text;
+						transform.translation = SFVec3f(0, 1, 0);
+                        transform.scale = SFVec3f(0.4, 0.4, 0.4);
+
+                        joints[i] = transform;
+                    }
+			}  
+    
+			function skeleton_changed(value, t)
+			{
+				cursor.render = true;
+                for(var i=0; i<24; ++i)
+                {
+                	//Set Z to zero to get a 2D-Cursor
+                    joints[i].transform = SFVec3f(value[i].x*20, value[i].y*20, 0);
+                }
+                //[11] is the right hand
+				new_pos = joints[11].transform;
+
+			}  
+
+			function shutdown()
+			{
+				//print("RightHand3DCursor END");
+			}    
