4d801527db5b077af5c802f2316e7a6e859b2374
Christoph Jobst <cjobst@wifa.uni-leipzig.de>
2013-04-05 01:15:37 +0200

    [feature] Gestureprinter

---
+++ src/Kinect SDK/GesturePrinter/GesturePrinterTest.js
@@ -0,0 +1,16 @@
+			function initialize() 
+			{
+				print("Gesture START");
+				
+			}  
+    
+			function gesture_changed(value, t)
+			{
+				print(value);
+
+			}  
+
+			function shutdown()
+			{
+				print("Gesture END");
+			}    
